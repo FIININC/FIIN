3ea62695611807e7e232e3e2d611a02154d37008
Kris Kerwin <kerwin.kristenm@gmail.com>
2018-05-20 14:49:11 -0400

    testing

--- js/header.js
+++ js/header.js
@@ -1,5 +1,40 @@
 ﻿
 $(document).ready(function(){
+
+
+    var win = $(window);
+
+    var allMods = $(".module");
+
+    allMods.each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            el.addClass("already-visible");
+        }
+    });
+
+    win.scroll(function(event) {
+
+        allMods.each(function(i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                el.addClass("come-in");
+            }
+        });
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
     var sections = $('section')
         , nav = $('nav')
         , nav_height = nav.outerHeight();
@@ -36,3 +71,25 @@ $(document).ready(function(){
 $(window).scroll(function(){
     $('header').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
+
+
+
+(function($) {
+
+    $.fn.visible = function(partial) {
+
+        var $t            = $(this),
+            $w            = $(window),
+            viewTop       = $w.scrollTop(),
+            viewBottom    = viewTop + $w.height(),
+            _top          = $t.offset().top,
+            _bottom       = _top + $t.height(),
+            compareTop    = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+    };
+
+})(jQuery);
+
